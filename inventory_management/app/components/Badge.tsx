30cba98695599d9f7072ead07342d91ccb1ee916
durimfe <durim.dev@gmail.com>
2023-09-06 18:54:26 +0200

    created badge

---
+++ inventory_management/app/components/Badge.tsx
@@ -0,0 +1,18 @@
+import { getStatusClassName } from "@/utils/functions";
+import React from "react";
+
+type Props = { text: string };
+
+export default function Badge({ text }: Props) {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div
+        className={`w-32 h-8 flex items-center justify-center rounded-md center text-white ${getStatusClassName(
+          text
+        )}`}
+      >
+        {text}
+      </div>
+    </div>
+  );
+}
