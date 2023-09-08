90c32cd93974b2975f10cb7a755d1fd349134f65
durimfe <durim.dev@gmail.com>
2023-09-08 12:55:04 +0200

    fixed table no-data cases

--- inventory_management/components/layout/Table.tsx
+++ inventory_management/components/layout/Table.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import ContainerHeader from "@/components/layout/container-header";
 import Link from "next/link";
 import mockData from "@/mock-api/data.json";
-import Button from "@/components/Button";
 import Chip from "@/components/Chip";
-import AddCategory from "./jobsite/add-category";
-import AddJobsite from "./jobsite/add-job";
+import useJobsites from "@/store/useJobsites";
+import { Icons } from "../Icons";
 
 interface Column {
   header: string;
@@ -17,16 +16,19 @@ interface Props {
   data: any[];
   columns: Column[];
   placeholder: string;
-  hasButton?: boolean;
+  children?: React.ReactNode;
+  isItem?: boolean;
 }
 
 export default function Table({
   data,
   columns,
   placeholder,
-  hasButton = true,
+  children,
+  isItem = false,
 }: Props) {
   const [filter, setFilter] = useState<string>("");
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const sanitizedInput = e.target.value.toLowerCase();
     setFilter(sanitizedInput);
@@ -35,6 +37,8 @@ export default function Table({
   const filteredData = data?.filter((item) =>
     item.name?.toLowerCase().includes(filter)
   );
+
+  console.log("filteredData", filteredData);
 
   return (
     <div className="flex flex-col gap-5 w-full shadow-md rounded-lg overflow-hidden bg-background">
@@ -49,12 +53,7 @@ export default function Table({
             className="w-full md:w-96 h-8 pl-10 pr-4 py-2 rounded-md border flex items-center bg-brand-background-secondary placeholder-[#EAEAEA]"
           />
         </div>
-        {hasButton && (
-          <>
-            <AddJobsite />
-            <AddCategory />
-          </>
-        )}
+        {children}
       </div>
       <table className="w-full">
         <thead>
@@ -67,68 +66,78 @@ export default function Table({
           </tr>
         </thead>
 
-        <tbody>
-          {data?.length === 0 ? (
-            <>
-              {Array.from({ length: mockData.jobsites?.length }).map(
-                (_, index) => (
+        <Suspense fallback={<p>Loading....</p>}>
+          <tbody>
+            {data?.length === 0 ? (
+              <>
+                <tr>
+                  <td
+                    className="text-center p-2 bg-white h-96 text-base font-semibold border-t"
+                    colSpan={columns.length}
+                  >
+                    <Icons.nodata className="w-full" />
+                    {isItem ? (
+                      <>
+                        <p className="mt-5">No Service Selected</p>
+                        <span className="font-normal">
+                          Please select a service on your left to proceed.
+                        </span>
+                      </>
+                    ) : (
+                      <p className="m-5">No data.</p>
+                    )}
+                  </td>
+                </tr>
+              </>
+            ) : filteredData?.length === 0 ? (
+              <>
+                <tr>
+                  <td
+                    className="text-center p-2 bg-brand-background-secondary"
+                    colSpan={columns.length}
+                  >
+                    {isItem ? (
+                      <Icons.nodata />
+                    ) : (
+                      <p>No results found for "{filter}".</p>
+                    )}
+                  </td>
+                </tr>
+              </>
+            ) : (
+              filteredData?.map((item, index) => {
+                return (
                   <tr
                     key={index}
-                    className={`text-center animate-pulse  h-[34px] ${
+                    className={`text-left h-[34px] ${
                       index % 2 === 0 ? "bg-brand-background-secondary" : ""
                     }`}
                   >
                     {columns.map((column, colIndex) => (
-                      <td key={colIndex}></td>
+                      <td
+                        key={colIndex}
+                        className={`pl-5 py-2 md:pl-[573px] md:py-0  ${column.className}`}
+                      >
+                        {column.key === "name" ? (
+                          <Link
+                            href={`/jobsites/${item.id}`}
+                            className="text-[#1264A3] text-left  font-semibold whitespace-break-spaces"
+                          >
+                            {item[column.key]}
+                          </Link>
+                        ) : column.key === "status" ? (
+                          <Chip text={item[column.key]} />
+                        ) : (
+                          item[column.key]
+                        )}
+                      </td>
                     ))}
                   </tr>
-                )
-              )}
-            </>
-          ) : filteredData?.length === 0 ? (
-            <>
-              <tr>
-                <td
-                  className="text-center p-2 bg-brand-background-secondary"
-                  colSpan={columns.length}
-                >
-                  No results found.
-                </td>
-              </tr>
-            </>
-          ) : (
-            filteredData?.map((item, index) => {
-              return (
-                <tr
-                  key={index}
-                  className={`text-left h-[34px] ${
-                    index % 2 === 0 ? "bg-brand-background-secondary" : ""
-                  }`}
-                >
-                  {columns.map((column, colIndex) => (
-                    <td
-                      key={colIndex}
-                      className={`pl-5 py-2 md:pl-[573px] md:py-0  ${column.className}`}
-                    >
-                      {column.key === "name" ? (
-                        <Link
-                          href={`/jobsites/${item.id}`}
-                          className="text-[#1264A3] text-left  font-semibold whitespace-break-spaces"
-                        >
-                          {item[column.key]}
-                        </Link>
-                      ) : column.key === "status" ? (
-                        <Chip text={item[column.key]} />
-                      ) : (
-                        item[column.key]
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })
-          )}
-        </tbody>
+                );
+              })
+            )}
+          </tbody>
+        </Suspense>
       </table>
     </div>
   );
