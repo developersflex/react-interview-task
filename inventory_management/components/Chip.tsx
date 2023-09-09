f280e4888446065dcfc8581d0f779e244c5e6aa0
durimfe <durim.dev@gmail.com>
2023-09-09 09:15:44 +0200

    added bg class to use with function

--- inventory_management/components/Chip.tsx
+++ inventory_management/components/Chip.tsx
@@ -7,7 +7,7 @@ export default function Chip({ text }: Props) {
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div
-        className={`w-32 h-8 flex items-center justify-center rounded-md center text-white ${getStatusClassName(
+        className={`w-32 h-8 flex items-center justify-center rounded-md center text-white bg-${getStatusClassName(
           text
         )}`}
       >
