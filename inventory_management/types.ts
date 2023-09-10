ce9b17384cfa888c8c5ba203520e43bfffd348b0
durimfe <durim.dev@gmail.com>
2023-09-10 15:16:19 +0200

    Update types

--- inventory_management/types.ts
+++ inventory_management/types.ts
@@ -8,12 +8,14 @@ export type Status = "Completed" | "In Progress" | "On Road" | "On Hold";
 export type Categories = "Sidewalk Shed" | "Scaffold" | "Shoring";
 
 export interface Jobsite {
+  id: number;
   name: string;
   status: string;
   categories: Categories[];
 }
 
 export type CategoryItem = {
+  id: number;
   item: string;
   quantity: string;
   description: string;
