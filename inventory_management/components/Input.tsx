a12a1b72dbc2fdc81f7d27561f33606d3f398370
durimfe <durim.dev@gmail.com>
2023-09-10 11:15:18 +0200

    created custom input

--- inventory_management/components/Input.tsx
+++ inventory_management/components/Input.tsx
@@ -1,26 +1,41 @@
+import { useField } from "formik";
 import React from "react";
 
-type Props = {
+type InputProps = {
   placeholder: string;
   label: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  className?: string;
 };
 
-function Input({ placeholder, label, onChange }: Props) {
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  label,
+  name,
+  className,
+}) => {
+  const [field, meta] = useField(name);
+  const hasError = meta.touched && meta.error;
+
   return (
-    <div className="flex flex-col gap-1 w-full">
-      <label className="ml-4 text-base font-semibold">{label}</label>
-      <input
-        type="text"
-        id={label}
-        name="name"
-        placeholder={placeholder}
-        required
-        className="rounded-md bg-brand-background-primary h-8 px-2 py-1"
-        onChange={onChange}
-      />
+    <div className={`w-full mb-5 ${className}`}>
+      <div className="relative flex flex-col gap-1">
+        <label className="ml-4 text-base font-semibold">{label}</label>
+        <input
+          type="text"
+          id={label}
+          placeholder={placeholder}
+          className="rounded-md bg-brand-background-primary placeholder-[#E0E0E1] h-8 px-2 py-1"
+          {...field}
+        />
+        {hasError && (
+          <p className="text-brand-red-1000 text-xs ml-2 absolute -bottom-5">
+            {meta.error}
+          </p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default Input;
