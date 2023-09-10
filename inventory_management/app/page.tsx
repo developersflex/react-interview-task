8aa02a16684c2360174a37daa9eda4c53b8e5552
durimfe <durim.dev@gmail.com>
2023-09-10 16:12:15 +0200

    removed function from useffect

--- inventory_management/app/page.tsx
+++ inventory_management/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const { get } = useJobsites();
   useEffect(() => {
     get();
-  }, [get]);
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center gap-[10px] ">
